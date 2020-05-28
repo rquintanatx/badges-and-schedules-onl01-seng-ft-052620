a07634c30abf24f6324563fd591b9109495bcb2c
Robert Quintana <robertquintanatx@gmail.com>
2020-05-28 06:53:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  message_array = []
+  name_array.each do |name| 
+    message = "Hello, my name is #{name}."
+    message_array << message
+  end
+  message_array
+end
+
+def assign_rooms(name_array)
+  room_assignment = 1 
+  room_message_array = []
+  name_array.each do |name| 
+    room_message = "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
+    room_message_array << room_message
+    room_assignment += 1
+  end
+  room_message_array
+end
+
+def printer(name_array)
+  badge_array = batch_badge_creator(name_array)
+  room_array = assign_rooms(name_array)
+  badge_array.each do |badge|
+    puts badge
+    puts room_array[badge.index]
+  end
+end
+    
+printer(["abe","abe1","abe2","abe3","abe4","abe5","abe6"])    
